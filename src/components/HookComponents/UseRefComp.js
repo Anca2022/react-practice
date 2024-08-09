433ab6dcf81a0c55ed9948beb3c2cb3725513bb8
Anca Matei <ancamatei2012@gmail.com>
2024-08-09 13:06:38 +0300

    useRef hook

--- src/components/HookComponents/UseRefComp.js
+++ src/components/HookComponents/UseRefComp.js
@@ -1,8 +1,32 @@
+import { useRef } from "react";
+import Stopwatch from "./extra/Stopwatch";
 export default function UseRefComp() {
+    const inputRef1=useRef(null);
+    const inputRef2=useRef(null);
+
+    function handleClick1(){
+        inputRef1.current.focus();
+        inputRef1.current.style.backgroundColor = 'pink';
+    }
+    function handleClick2(){
+        inputRef2.current.focus();
+        inputRef2.current.style.backgroundColor = 'pink';
+    }
+
     return(
         <div>
-            <h2>useRef Hook</h2>
-        
+            <h2>useRef Hook - Classic use case: </h2>
+            <div>
+                <button onClick={handleClick1}>Click to focus input1</button> 
+                <br></br>
+                <input ref={inputRef1} type='text' onBlur={()=> inputRef1.current.style.backgroundColor = ''}/> 
+                <br></br>
+                <button onClick={handleClick2}>Click to focus input2</button> 
+                <br></br>
+                <input ref={inputRef2} type='text' onBlur={()=> inputRef2.current.style.backgroundColor = ''} /> 
+            </div>
+            <h2>+ Stopwatch</h2>
+            <Stopwatch />
         </div>
     );
 }
