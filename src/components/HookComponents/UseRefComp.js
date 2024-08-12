10471237f942991026b137f86960fd4bed101494
Anca Matei <ancamatei2012@gmail.com>
2024-08-12 10:40:52 +0300

    useReducer hook

--- src/components/HookComponents/UseRefComp.js
+++ src/components/HookComponents/UseRefComp.js
@@ -4,26 +4,25 @@ export default function UseRefComp() {
     const inputRef1=useRef(null);
     const inputRef2=useRef(null);
 
-    function handleClick1(){
-        inputRef1.current.focus();
-        inputRef1.current.style.backgroundColor = 'pink';
+    function handleClick(input){
+        input.current.focus();
+        input.current.style.backgroundColor = 'pink';
     }
-    function handleClick2(){
-        inputRef2.current.focus();
-        inputRef2.current.style.backgroundColor = 'pink';
+    function handleOnBlur(input){
+       input.current.style.backgroundColor = '';
     }
 
     return(
         <div>
-            <h2>useRef Hook - Classic use case: </h2>
+            <h2>useRef Hook - Common use case: </h2>
             <div>
-                <button onClick={handleClick1}>Click to focus input1</button> 
+                <button onClick={()=>handleClick(inputRef1)}>Click to focus input1</button> 
                 <br></br>
-                <input ref={inputRef1} type='text' onBlur={()=> inputRef1.current.style.backgroundColor = ''}/> 
+                <input ref={inputRef1} type='text' onBlur={()=> handleOnBlur(inputRef1)} /> 
                 <br></br>
-                <button onClick={handleClick2}>Click to focus input2</button> 
+                <button onClick={()=>handleClick(inputRef2)}>Click to focus input2</button> 
                 <br></br>
-                <input ref={inputRef2} type='text' onBlur={()=> inputRef2.current.style.backgroundColor = ''} /> 
+                <input ref={inputRef2} type='text' onBlur={()=> handleOnBlur(inputRef2)} /> 
             </div>
             <h2>+ Stopwatch</h2>
             <Stopwatch />
